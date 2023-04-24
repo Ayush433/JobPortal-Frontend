656ec4cdda901daa1af4e6c636676ce6a7f0bdde
Ayush Adhikari <adhikariayush19@gmail.comgit config --global user.nameAyush433>
2023-04-24 10:14:17 +0545

    Navbar and Header part added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   }, [theme]);
   return (
     <div>
-      <nav className="bg-blue-400 border-gray-200 dark:bg-gray-900">
+      <nav className="bg-blue-700 border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="https://flowbite.com/" className="flex items-center">
             <img
@@ -152,6 +152,14 @@ const Navbar = () => {
                   Login
                 </a>
               </li>
+              {/* <li>
+                <a
+                  href="#"
+                  className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                >
+                  Profile
+                </a>
+              </li> */}
               <li>
                 <a
                   href="#"
